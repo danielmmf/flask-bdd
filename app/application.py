893f4da482aa50de2ff1ee4acd0ef898b40c5dce
Daniel Medeiros <danielmmf@gmail.com>
2020-04-07 04:34:33 -0300

    trabalhando com python

--- app/application.py
+++ app/application.py
@@ -12,11 +12,14 @@ def hello_world():
 @app.route("/user/<username>", methods=[GET])
 def access_users(username):
     if request.method == GET:
-        user_details = USERS.get(username)
-        if user_details:
-            return jsonify(user_details)
-        else:
-            return Response(status=404)
+		USERS.update({'david01': {'name': 'David Sale'}})
+
+		user_details = USERS.get(username)
+
+		if user_details:
+			return jsonify(user_details)
+		else:
+			return Response(status=404)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='5000', debug=True)
